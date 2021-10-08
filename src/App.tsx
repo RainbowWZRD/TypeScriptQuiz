b331ff0885eaaf0648d20376239da8b71a07117d
RNBW WZRD <wzrd@MBP-di-RNBW.station>
2021-10-08 17:17:22 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
+
+  const startTrivia = async () => {
+
+  }
+
+  //Always need to specify the type
+  const checkAnswer = (e:React.MouseEvent<HTMLButtonElement> ) => {
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <p style={{fontFamily : "Poppins"}}>Quiz</p>
+          </div>
   );
 }
 
