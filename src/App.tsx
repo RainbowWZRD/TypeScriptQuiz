395a4ebd6535fc345bf42e4353e53ab244f0c321
RNBW WZRD <wzrd@MacBook-Pro-di-RNBW.local>
2021-10-09 14:58:16 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
-import React from "react"
+import React, {useState} from "react"
+import QuestionCard from "./components/QuestionCard"
+import {fetchQuizQuestion} from "./API"
+import { isTokenKind } from "typescript"
+import {Difficulty} from "./API"
 
 function App() {
+const [loading, setLoading] = useState(false)
+const [questions, setQuestions] = useState([])
+const [number, setNumber] = useState(0)
+const [userAnswer, setUserAnswer] = useState([])
+const [score, setScore] = useState(0)
+const [gameOver, setGameOver] = useState(true)
 
-  const startTrivia = async () => {
+const TOTAL_QUESTION = 10
 
-  }
+console.log(fetchQuizQuestion(TOTAL_QUESTION, Difficulty.EASY)
+
+
+  const startTrivia = async () => {}
 
   //Always need to specify the type
-  const checkAnswer = (e:React.MouseEvent<HTMLButtonElement> ) => {
+  const checkAnswer = (e:React.MouseEvent<HTMLButtonElement> ) => {}
 
-  }
+  const nextQuestion = ( ) => {}
 
   return (
     <div className="App">
-      <p style={{fontFamily : "Poppins"}}>Quiz</p>
+      <p style={{fontFamily : "Poppins"}}>REACT QUIZ</p>
+      <button onClick={startTrivia}>Start</button>
+      <p className="score">Score:</p>
+      <p>Loading questions</p>
+      {/* <QuestionCard 
+      question={questions[number].question} 
+      answers={questions[number].answers} 
+      callback={checkAnswer} 
+      userAnswer={userAnswer ? userAnswer[number] : undefined}  
+      questionNr={number + 1} 
+      totalQuestions={TOTAL_QUESTION} /> */}
+      <button onClick={nextQuestion}>Next</button>
           </div>
   );
 }
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,3 +1,38 @@
 import React from "react";
+import { callbackify } from "util";
 
-const   QuestionCard = () => <div>Question Card</div>
+type Props = {
+    question : string,
+    answers : string[],
+    callback : any,
+    userAnswer : any,
+    questionNr : number,
+    totalQuestions : number
+}
+
+const QuestionCard : React.FC<Props> = ({
+    question,
+    answers,
+    userAnswer,
+    questionNr,
+    totalQuestions
+}) => {
+    return <>
+    <p className="number">
+        Question : {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{__html: question}}></p>
+    <div>
+        {answers.map((answer) => {
+            <div>
+                <button disabled={userAnswer} 
+                >
+                    <span dangerouslySetInnerHTML={{__html: answer}} />
+                </button>
+                </div>
+        })}
+    </div>
+    </>
+}
+
+export default QuestionCard
